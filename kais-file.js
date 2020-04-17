466d91303f96b651518158850c27fe65426cc4b3
KaiErfle <kaierfle@gmail.com>
2020-04-17 13:02:57 -0700

    test of menu item

--- kais-file.js
+++ kais-file.js
@@ -1,4 +1,18 @@
-var bgstate = false
-
-function bgstate()
-     
+var roundButton = document.querySelector("#roundButton");
+roundButton.addEventListener("click", showMenu, false);
+ 
+var flyoutMenu = document.querySelector("#flyoutMenu");
+flyoutMenu.addEventListener("click", hideMenu, false);
+ 
+function showMenu(e) {
+    flyoutMenu.classList.add("show");
+ 
+    document.body.style.overflow = "hidden";
+}
+ 
+function hideMenu(e) {
+    flyoutMenu.classList.remove("show");
+    e.stopPropagation();
+ 
+    document.body.style.overflow = "auto";
+} 
